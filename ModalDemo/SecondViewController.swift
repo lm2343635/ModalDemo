023676d9ec265451cb38eb535c23ab87e1fd6129
Meng Li <lm2343635@126.com>
2018-12-18 18:18:14 +0900

    Add demo.

---
+++ ModalDemo/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  ModalDemo
+//
+//  Created by mon.ri on 2018/12/18.
+//  Copyright © 2018 MuShare. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func back(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+}
